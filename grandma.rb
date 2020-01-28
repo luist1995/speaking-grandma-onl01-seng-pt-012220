87092a1c742f028cd6211a8067cb2f2c25a47f5e
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-01-28 02:55:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (phrase = "I LOVE YOU GRANDMA!")
-  if String == phrase.downcase
+  if String != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif String != phrase
     return "NO, NOT SINCE 1938!"
