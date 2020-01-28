9cc517443eb0c51b2f50e73ab8c8f7da1d67ae4a
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-01-28 02:58:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (phrase = "I LOVE YOU GRANDMA!")
   if String == "Hi Nana, How are you?"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif String != phrase
+  elsif String == phrase
     return "NO, NOT SINCE 1938!"
   else 
     return "I LOVE YOU TOO PUMPKIN!"
